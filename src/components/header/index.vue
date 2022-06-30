1be392489f3432f366927ec62ccae06237f7403e
wf0 <1982392655@qq.com>
2022-06-30 11:50:09 +0800

    修改header组件的搜索框功能

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -6,6 +6,7 @@
         alt="logo">
     </div>
     <div class="menu">
+      <el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="input2" size="mini" />
       <el-button size="mini" @click="toApache('document')" icon="el-icon-tickets">文档</el-button>
       <el-button size="mini" @click="toApache('help')" icon="el-icon-search">帮助</el-button>
       <el-button size="mini" @click="toApache('github')" icon="el-icon-search">Github</el-button>
@@ -28,7 +29,7 @@
 
 <script>
 export default {
-  name:"Myheader",
+  name: "Myheader",
   methods: {
     toApache(type) {
       switch (type) {
@@ -71,6 +72,12 @@ export default {
 }
 </script>
 <style scoped>
+.el-input{
+  display: inline-block;
+  width: 150px;
+  margin: 0 10px 0 0 ;
+
+}
 .el-dropdown {
   margin: 0 0 0 10px;
 }
